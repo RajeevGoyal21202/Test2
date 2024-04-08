f480c8f4881c8fcf2f4bdbd043e8d2f0a79f6ca7
RajeevGoyal2120 <rajeev.1149@zenmonk.tech>
2024-04-08 19:01:10 +0530

    routes protected

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './style/app.css';
-import { Routes,Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import AddQuiz from './pages/Admin/AddQuiz';
 import AdminDashboard from './pages/Admin/Dashboard';
 import AddQuestion from './pages/Admin/AddQuestion';
@@ -8,19 +8,21 @@ import Signup from './pages/common/signup';
 import Login from './pages/common/login';
 import AdminRoute from './component/Routes/AdminRoutes';
 import ViewQuestion from './pages/Admin/ViewQuestions';
+import PrivateRoutes from './component/PrivateRoute';
 function App() {
   return (
-   <Routes>
-    <Route path='/' element={<Login/>}/>
-    <Route path='/signup' element={<Signup/>}/>
-    <Route path='/addQuiz' element={<AddQuiz/>}/>
-    <Route path='/adminDashboard' element={<AdminDashboard/>}/>
-    <Route path="/home" element={<HomePage/>}/>
-    <Route path='/addQuestion' element={<AddQuestion/>}/>
-    <Route path='/viewQues' element={<ViewQuestion/>}/>
-    
-  
-   </Routes>
+    <Routes>
+      <Route path='/' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route element={<PrivateRoutes />}>
+        <Route path='/addQuiz' element={<AddQuiz />} />
+        <Route path='/adminDashboard' element={<AdminDashboard />} />
+        <Route path='/addQuestion' element={<AddQuestion />} />
+        <Route path='/viewQues' element={<ViewQuestion />} />
+      </Route>
+
+
+    </Routes>
   );
 }
 
